1b56aaaccbbb44e210388d4acb4fec82424074ec
unknown <mutiarannisa877@gmail.com>
2024-12-20 10:49:29 +0700

    second commit add table coloum index.php

--- index.php
+++ index.php
@@ -3,10 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Latihan GIT</title>
+    <title>Latian Git</title>
 </head>
 <body>
     <h1>Latihan git</h1>
+    <tr>
+        <td>Nama</td>
+        <td>Kelas</td>
+    </tr>
+    <tr>
+        <td>Kareen</td>
+        <td>Kelas XML</td>
+    </tr>
     <button>Submit</button>
 </body>
 </html>
